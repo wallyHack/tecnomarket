784493179499e017f0bc2586227b296da87b9852
Manuel Herrera Lara <m.herreralara80@gmail.com>
2020-11-26 19:54:53 -0600

    creamos los modelos y los registramos en el admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Producto, Marca
 
 # Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    search_fields = ['nombre']
+    list_display = ['nombre', 'precio', 'descripcion', 'nuevo', 'marca', 'fecha_de_fabricacion']
+    list_filter = ['fecha_de_fabricacion']
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Marca)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=3)
+    descripcion = models.TextField()
+    nuevo = models.BooleanField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_de_fabricacion = models.DateField()
+
+    def __str__(self):
+        return self.nombre 
