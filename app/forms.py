9d3654c1e44c28d0c5b8ff7e540b179442970b53
Manuel Herrera <m.herreralara80@gmail.com>
2020-11-29 01:15:51 -0600

    integramos bootstrap al proyecto y agregamos estilos a los elementos del formulario con el plugin crispy

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from .models import Contacto
 
 class ContactoForm(forms.ModelForm):
+
+    # agregando estilos a elementos por separado
+    # nombre = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+
     class Meta:
         model = Contacto
         # fields = ['nombre', 'correo', 'telefono', 'tipo_de_consulta', 'mensaje', 'avisos']
