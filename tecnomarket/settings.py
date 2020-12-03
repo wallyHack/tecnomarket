d6942adfbcf1a62c3dbfc4f17298f4339d534477
Manuel Herrera <m.herreralara80@gmail.com>
2020-12-03 01:12:30 -0600

    creamos el formulario de login usando el sistema de autenticación por default de django

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -18,6 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # usando mensajes o nitificaciones
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
