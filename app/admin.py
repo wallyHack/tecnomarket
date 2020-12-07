0c69d5068fae30dbdcab36b6fb91156c7933f1e7
Manuel Herrera <m.herreralara80@gmail.com>
2020-12-06 23:48:35 -0600

    usamos el sistema de autorización y permisos

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from .models import Producto, Marca, Contacto
 
 # Register your models here.
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'precio', 'descripcion', 'nuevo', 'marca', 'fecha_de_fabricacion']
+    list_display = ['id', 'nombre', 'precio', 'descripcion', 'nuevo', 'marca', 'fecha_de_fabricacion']
     list_editable = ['nuevo']  # columna editable
     search_fields = ['nombre'] # busqueda por columna   
     list_filter = ['marca', 'nuevo', 'fecha_de_fabricacion'] # filtro por marca y nuevo
@@ -12,7 +12,7 @@ class ProductoAdmin(admin.ModelAdmin):
 admin.site.register(Producto, ProductoAdmin)
 
 class ContactoAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'correo', 'telefono', 'tipo_de_consulta', 'mensaje', 'avisos']
+    list_display = ['id', 'nombre', 'correo', 'telefono', 'tipo_de_consulta', 'mensaje', 'avisos']
     list_editable = ['tipo_de_consulta']
     search_fields = ['nombre']
     list_filter = ['nombre', 'tipo_de_consulta']
--- app/views.py
+++ app/views.py
@@ -5,13 +5,16 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required, permission_required
 
 # Create your views here.
+@login_required
 def home(request):
     lista_de_productos = Producto.objects.all()
     context = {'lista_de_productos': lista_de_productos}
     return render(request, 'app/home.html', context)
 
+@login_required
 def contacto(request):
     # creamos una instancia con el formulario vacio
     data = {
@@ -28,9 +31,11 @@ def contacto(request):
 
     return render(request, 'app/contacto.html', data)
 
+@login_required
 def galeria(request):
     return render(request, 'app/galeria.html')
 
+@permission_required('app.add_producto')
 def agregar_producto(request):   
     data = {
         'form': ProductoForm()
@@ -47,6 +52,7 @@ def agregar_producto(request):
     
     return render(request, 'app/producto/agregar.html', data)
 
+@permission_required('app.view_producto')
 def lista_de_productos(request):
     productos = Producto.objects.all()
     page = request.GET.get('page', 1)
@@ -65,6 +71,7 @@ def lista_de_productos(request):
     return render(request, 'app/producto/listar.html', data)
 
 
+@permission_required('app.change_producto')
 def modificar_producto(request, id):    
     # primero buscamos el producto
     producto = get_object_or_404(Producto, pk=id)
@@ -84,6 +91,7 @@ def modificar_producto(request, id):
 
     return render(request, 'app/producto/modificar.html', data)
 
+@permission_required('app.delete_producto')
 def eliminar_producto(request, id):
     # primero buscamos el producto
     producto = get_object_or_404(Producto, pk=id)
