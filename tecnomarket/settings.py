3a1ec8c5f573753213e371428f6747e24d4f5141
Manuel Herrera <52230895+wallyHack@users.noreply.github.com>
2021-10-21 00:26:17 -0500

    configuramos los ajustes para realizar la creación automatica de llaves primarias en los modelos

--- tecnomarket/settings.py
+++ tecnomarket/settings.py
@@ -15,11 +15,14 @@ from pathlib import Path, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# usando mensajes o nitificaciones
+# usando mensajes o notificaciones
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# omitimos la creación automatica de llaves primarias
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # vinculación de autencitación con facebook
 SOCIAL_AUTH_FACEBOOK_KEY = "1294829714209101"
